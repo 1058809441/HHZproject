3988dba4a41e4285e6dda020f87a77915b7e5a4e
1058809441 <1058809441@qq.com>
2019-09-22 15:59:15 +0800

    hpx更新于2019/9/22-16:00

--- server/Models/ShoppingCarModel.js
+++ server/Models/ShoppingCarModel.js
@@ -6,6 +6,18 @@ class ShoppingCarModel extends SqlBase {
         super();
     }
 
+    addToShoppingCar(data, callback) {
+        console.log("addToShoppingCar");
+        let sql = `insert into shopping(username,productId,productName,productPrice,productCount,productType,productImg) values('${data.username}',${data.productID},'${data.productName}',${data.productPrice},1,'${data.productType}','${data.productImg}')`;
+        this.connection.query(sql, function (err, result) {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            callback(result);
+        });
+    }
+
     deleteFromShopping(data, callback) {
         let sql = ` delete from shopping where username='${data.username}' and productId=${data.productID}`;
         this.connection.query(sql, function (err, result) {
@@ -32,7 +44,8 @@ class ShoppingCarModel extends SqlBase {
 
     //根据用户名和商品ID搜索商品数量
     selectProductCount(data, callback) {
-        let sql = ` select productCount from shopping where username='${data.username}' and where productId=${data.productID}`;
+        // console.log("selectProductCount");
+        let sql = ` select productCount from shopping where username='${data.username}' and productId=${data.productID}`;
         this.connection.query(sql, function (err, result) {
             if (err) {
                 console.log(err.message);
@@ -48,16 +61,18 @@ class ShoppingCarModel extends SqlBase {
     //修改指定购物车信息的商品数量
     updateToShoppingCar(data, callback) {
         var sql = "";
+        // console.log("updateToShoppingCar");
+        // console.log(data.productID);
         if (data.tag == 1) {
             data.oldCount++;
-            sql = `update shopping set count=${data.productCount} where productId=${data.productID}`;
+            sql = `update shopping set productCount=${data.oldCount} where productId=${data.productID}`;
         } else if (data.oldCount <= 1) {
             callback("数量为1，不能再减少了，选择删除操作才能减为1");
             return
         }
         else if (data.tag == -1 && data.oldCount > 1) {
             data.oldCount--;
-            sql = `update shopping set count=${data.productCount} where productId=${data.productID}`;
+            sql = `update shopping set productCount=${data.oldCount} where productId=${data.productID}`;
         }
         this.connection.query(sql, function (err, result) {
             if (err) {
